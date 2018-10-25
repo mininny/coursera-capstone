b22f5f0c6b2345b8b705d2956bd37247ff64379f
Minhyuk Kim <minhyukkim@Minhyuks-MacBook-Pro.local>
2018-10-26 01:51:11 +0900

    Final

--- app.py
+++ app.py
@@ -1,53 +1,37 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, Flask, send_file
+    flash, g, redirect, render_template, request, session, url_for, Flask
 )
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from flask_bcrypt import Bcrypt
 from passlib.hash import sha256_crypt
 import os
-import functools
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators, SubmitField
 from cryptography.fernet import Fernet
-from werkzeug.security import check_password_hash, generate_password_hash
 import uuid
-import mysql.connector
 import sqlite3
 from sqlalchemy import create_engine
 import tempfile
 import zipfile
 from time import gmtime, strftime
-
+import base64
 app = Flask(__name__)
-# app.secret_key = str(uuid.uuid4())
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_DB'] = 'cryptoApp'
-# app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-# app.config['MYSQL_PASSWORD'] = '123456'
-app.secret_key = "verysecret"
-PATH = os.path.join(tempfile.gettempdir(), 'init.db')
-# engine = create_engine("sqlite:////data/test.db")
 
-# flask_sqlalchemy.sqlalchemy.create_engine("sqlite:///data/database.db")
+app.secret_key = "verysecret"
+PATH = os.path.join(tempfile.gettempdir(), 'testestesbase.db')
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"+PATH
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-# sql = mysql.connector.connect(
-#     host ="localhost",
-#     user = "root",
-#     passwd = "1234",
-#     database = "cryptoApp"
-# )
+
 @login_manager.user_loader
 def load_user(username):
     return User.query.filter_by(username = username).first()
 
 class User(db.Model):
     __tablename__ = "users"
-    username = db.Column(db.String(80), unique=True, primary_key=True)
-    password = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(80), unique=True, primary_key=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
     def __init__(self, username, password):
         self.username = username
         self.password = password
@@ -62,34 +46,23 @@ class User(db.Model):
         
 class Chat(db.Model):
     __tablename__ = "chats"
-    sender = db.Column(db.String(80))#, nullable=False)
-    chat = db.Column(db.String(120))#, nullable=False)
-    receiver = db.Column(db.String(80))#, nullable=False)
+    sender = db.Column(db.String(256))
+    chat = db.Column(db.String(256))
+    receiver = db.Column(db.String(80))
     time = db.Column(db.String(30), primary_key=True)
     def __init__(self, sender, receiver, chat):
         self.sender = sender
         self.receiver = receiver
         self.chat = chat
         self.time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        print("Sending chat from %s to %s", (sender, receiver))
-#     def __init__(self, sender, chat, receiver):
-#         self.sender = sender
-#         self.chat = chat
-#         self.receiver = receiver
 
 db.create_all()
-def initDB():
-    db.init_app(app)
-    db.app = app
-    db.create_all()
 
 
-# initDB()
+
 app.debug = True
 print("INITIZLIAED")
-encrypt = Fernet(Fernet.generate_key())
-# db.Query.addcolumn()
-
+encrypt = Fernet(b'CC9R5uGZBs8obPmSnPnIipZek-Rfox2lzDm-8zCSTgE=')
 
 @app.route('/')
 def index():
@@ -101,78 +74,88 @@ def login():
     form = UserForm(request.form)
     if request.method == 'POST':
         username = request.form['username']
-        password = request.form['password']#form.password.data
+        password = request.form['password']
         usernameSalt = str(uuid.uuid4())
-        print(username)
         results = db.engine.execute("SELECT * FROM users WHERE username='{0}'".format(username))
         for result in results:
-            # Get stored hash
-            print(result)
             data = result
             password_candidate = data['password']
-            user = User.query.filter_by(username=form.username.data).first()
-            # Compare Passwords
-            if password_candidate == password:
-                # Passed
-                session['logged_in'] = True
-                session['username'] = username
+            if sha256_crypt.verify(password, password_candidate):
+                user = User.query.filter_by(username=form.username.data).first()
                 login_user(user)
-                flash('You are now logged in', 'success')
-                print("Logged In'")
+                flash('Successfully logged in', 'success')
                 return redirect(url_for('inbox'))
             else:
-                error = 'Invalid login'
-                print("Wrong password")
-                return render_template('login.html', error=error)
-            # Close connection
-        error = 'Username not found'
-        print("Username not found while logging in")
-        return render_template('login.html', error=error)
-        # encryptedUsernameSalt = encrypt.encrypt(usernameSalt)
-        # passwordHash = Bcrypt.generate_password_hash(usernameSalt+"#"+form.password)
-        # flash("Loggin In")
-        
-        # login_user(user)
-        # return redirect(url_for("inbox"))
-        # return render_template("/register.html")
+                flash("Wrong Password", "error")
+                return render_template('login.html', form=form)
+        flash("No matching username", "error")
+        return render_template('login.html', form=form)
     return render_template('/login.html', form=form)
 
 @app.route("/logout")
 def logout():
-    if session["logged_in"]:
-        session.clear()
-        flash("Logged out of service", 'success')
+    if current_user.is_authenticated:
+        logout_user()
+        flash("Logging out of service", 'success')
         return redirect(url_for('login'))
     else:
-        flash("Already logged out", 'failure')
         return redirect(url_for('login'))
 
-@app.route('/inbox', methods=('GET','POST'))
+@app.route('/inbox', methods=('GET', 'POST'))
 def inbox():
     form = chatForm(request.form)
     if request.method=='GET':
         if current_user.is_authenticated:
             user = current_user.get_id()
             chatList = Chat.query.filter_by(receiver = user).all()
-        return render_template('/inbox.html', form=form, chatList = chatList)
+            decryptedChatList = []
+            for chat in chatList:
+                print(str(chat.sender))
+                decodedSender = encrypt.decrypt(bytes(str(chat.sender), 'utf-8')[2:-1]).decode('utf-8')
+                print(decodedSender)
+                decodedChat = encrypt.decrypt(bytes(str(chat.chat), 'utf-8')[2:-1]).decode('utf-8')
+                decryptedChat = Chat(decodedSender, str(chat.receiver), decodedChat)
+                decryptedChatList.append(decryptedChat)
+            return render_template('/inbox.html', form=form, chatList = decryptedChatList)
+        else:
+            logout()
+            flash("Not authenticated. Logging out", "error")
+            return redirect(url_for("login"))
     if request.method=='POST':
         if current_user.is_authenticated:
-            user = current_user.get_id()
-            chatList = Chat.query.filter_by(receiver = user).all()
-        return render_template('/inbox.html', form=form)
-    return render_template('/inbox.html')
+            return render_template('/inbox.html', form=form)
+        else:
+            logout()
+            return render_template('/login.html')
+    return render_template('/inbox.html')        
 
-@app.route('/outbox', methods=('GET', 'POST'))
+@app.route('/outbox', methods=('GET','POST'))
 def outbox():
     form = chatForm(request.form)
     if request.method == "POST":
         if current_user.is_authenticated:
-            receiver = form.receiver.data
-            chat = form.chat.data
-            newChat = Chat(current_user.get_id(), receiver, chat)
-            db.session.add(newChat)
-            db.session.commit()
-            return redirect(url_for("inbox"))
+            if form.receiver.data and form.chat.data:
+                receiver = form.receiver.data
+                chat = encrypt.encrypt(bytes(str(form.chat.data), 'utf-8'))
+                newChat = Chat(encrypt.encrypt(bytes(str(current_user.get_id()), 'utf-8')), receiver, chat)
+                db.session.add(newChat)
+                db.session.commit()
+                flash("Successfully sent message", "success")
+                return redirect(url_for("inbox"))
+            else:
+                flash("receiver of chat doesn't exist", "error")
+                return redirect(url_for('outbox'))
+        else:
+            flash("Not Authenticated!", "error")
+            logout()
+            return render_template("/outbox.html")
+    if request.method == "GET":
+        if current_user.is_authenticated:
+            return render_template("/outbox.html", form=form)
+        else:
+            flash("Not Authenticated!", "error")
+            logout()
+            return redirect(url_for("login"))
     return render_template("/outbox.html")
 
 
@@ -180,53 +163,30 @@ def outbox():
 def register():
     form = UserForm(request.form)
     if request.method == 'POST' and form.validate():
-        username = form.username.data
-        password = form.password.data
-        usernameSalt = str(uuid.uuid4())
-        # encryptedUsernameSalt = encrypt.encrypt(usernameSalt)
-        # passwordHash = Bcrypt.generate_password_hash(usernameSalt+"#"+form.password)
-        newUser = User(username, password)
-        # db.engine.execute("INSERT INTO users(username, password) VALUES('{0}','{1}')".format(username, password))
-        db.session.add(newUser)
-        db.session.commit()
-
-        # sql.commit()
-        # cur.close()
-        flash("LMAO")
-
-        return redirect(url_for("login"))
-        # if not username:
-        #     error = 'Username is required.'
-        # elif not password:
-        #     error = 'Password is required.'
-        # elif db.execute(
-        #     'SELECT id FROM user WHERE username = ?', (username,)
-        # ).fetchone() is not None:
-        #     error = 'User {} is already registered.'.format(username)
-
-        # if error is None:
-        #     db.execute(
-        #         'INSERT INTO user (username, password) VALUES (?, ?)',
-        #         (username, generate_password_hash(password))
-        #     )
-        #     db.commit()
-        #     return redirect(url_for('auth.login'))
+        if not User.query.filter_by(username=form.username.data).first():
+            username = form.username.data
+            password = sha256_crypt.encrypt(str(form.password.data))
+            newUser = User(username, password)
+            db.session.add(newUser)
+            db.session.commit()
+            flash("Successfully Registered", "success")
+            return redirect(url_for("login"))
+        else:
+            flash("Same Username already exists!", "error")
+            return render_template('/register.html', form=form)
     return render_template('/register.html', form=form)
 
 @app.route("/dbdump", methods=["GET"])
 def dbdump():
     if request.method == "GET":
-        # get DB dump in memory
         conn = sqlite3.connect(PATH)
         dump_data = '\n'.join(conn.iterdump())
         conn.close()
-        # zip the dump data into a file
         dumpfile = str(uuid.uuid4())+".dump.zip"
         zfile = zipfile.ZipFile(dumpfile, mode="w", compression=zipfile.ZIP_DEFLATED)
         zfile.writestr("dump.sql", dump_data)
         zfile.close()
         return send_file(dumpfile, as_attachment=True)
-    # handle other request methods
     else:
         error = "Method not allowed"
         return render_template("index.html", error = error)
